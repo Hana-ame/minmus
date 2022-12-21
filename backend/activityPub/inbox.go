aaa7268ed419a696041abd530d4cdc5972a68b5c
hana-ame <luminovoez@outlook.com>
2022-12-21 16:42:37 +0800

    things about crypto

--- backend/activityPub/inbox.go
+++ backend/activityPub/inbox.go
@@ -1,32 +1,35 @@
 package activityPub
 
 import (
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"net/http"
 
-	"github.com/fatih/color"
 	"github.com/hana-ame/minmus/backend/utils"
 )
 
 // /{username}/inbox
 // /{username}/inbox/
 func Inbox(w http.ResponseWriter, r *http.Request) {
-	key := utils.GenerateKey("")
-	color.HiMagenta(fmt.Sprintf("%s", *key))
-	keyBytes := utils.MarshalPublicKey(key)
-	color.HiMagenta(string(keyBytes))
+	pk, err := utils.ReadKeyFromFile("privatekey.pem")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(pk)
+	// key := utils.GenerateKey("")
+	// color.HiMagenta(fmt.Sprintf("%s", *key))
+	// keyBytes := utils.MarshalPublicKey(key)
+	// color.HiMagenta(string(keyBytes))
 	// return
 
-	msg := "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvHKTk7Nm8TFF2RUX+MwQ\n69hPKqcKE4JcRVx8eJW5ApSlQs4pu58wQO7yD+QvYIb4IKKOajQhKaaf3vCR0SWm\nwHLVtFn2Iwzm22VFR897eIKCN8DFGT4Nkq3/BiX8ZXb4NbtX5hry/Bj7b2MbDgfX\nptcoafxjofHUkqyZ5VcwgT+ctPjqAaiAHYmnxq4HPoxX5quuvNAe8Gl2Ij+ErE5v\n8Z/jXTMkOQJ0TBPCCSB/PTF0SSPRlwbyZVxXx/dTS/P5ewBO5KPJk9Ii/TDXSDFk\nJKmXxVd7kUIszAQ4qvg97mIi/L8TQf4g6D4Cj0EpbtRZnpv59vvoN7AcrgmKo0CI\n6J08wen9pGpiBp/gXs83mx6YY63SIqLlWEGpEgRZnrpe4nW24N0G1puiTSAiG3o2\n+loXh3J7b4fbcC/UR0pxLLtnhcYXhxUEyfb0d8DV8KjC4GlX9eiDmx0P5fSsUPdc\nOfHeKCKMLnI1FYbTEn+Ed7dYG4RwkXgzJPpigwHIWO2oqaGqWGt2UeOTr+HzbTqW\nOKw+tk1M/3X451DjWTmoGzLPDdoakB8pFyQ8YrrUcpUpLn9n4Susr2deek3UwfgK\nl23OwSttCkw4vs6+p063X3g0NwhWvZYDS/hJDJNbs33LHErfsiUidAmz8yEMsY0X\nKtdeL3dq3dvXYKwyHQ59zJcCAwEAAQ==\n-----END PUBLIC KEY-----\n"
-	block, _ := pem.Decode([]byte(msg))
-	color.HiMagenta(string(block.Type))
-	// color.HiMagenta(string(block.Bytes))
-	publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		color.Red(err.Error())
-	}
-	color.HiMagenta(fmt.Sprintf("%s", publicKey))
-	// color.HiMagenta(fmt.Sprintf("%T", publicKey)) // *rsa.PublicKey
+	// msg := "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvHKTk7Nm8TFF2RUX+MwQ\n69hPKqcKE4JcRVx8eJW5ApSlQs4pu58wQO7yD+QvYIb4IKKOajQhKaaf3vCR0SWm\nwHLVtFn2Iwzm22VFR897eIKCN8DFGT4Nkq3/BiX8ZXb4NbtX5hry/Bj7b2MbDgfX\nptcoafxjofHUkqyZ5VcwgT+ctPjqAaiAHYmnxq4HPoxX5quuvNAe8Gl2Ij+ErE5v\n8Z/jXTMkOQJ0TBPCCSB/PTF0SSPRlwbyZVxXx/dTS/P5ewBO5KPJk9Ii/TDXSDFk\nJKmXxVd7kUIszAQ4qvg97mIi/L8TQf4g6D4Cj0EpbtRZnpv59vvoN7AcrgmKo0CI\n6J08wen9pGpiBp/gXs83mx6YY63SIqLlWEGpEgRZnrpe4nW24N0G1puiTSAiG3o2\n+loXh3J7b4fbcC/UR0pxLLtnhcYXhxUEyfb0d8DV8KjC4GlX9eiDmx0P5fSsUPdc\nOfHeKCKMLnI1FYbTEn+Ed7dYG4RwkXgzJPpigwHIWO2oqaGqWGt2UeOTr+HzbTqW\nOKw+tk1M/3X451DjWTmoGzLPDdoakB8pFyQ8YrrUcpUpLn9n4Susr2deek3UwfgK\nl23OwSttCkw4vs6+p063X3g0NwhWvZYDS/hJDJNbs33LHErfsiUidAmz8yEMsY0X\nKtdeL3dq3dvXYKwyHQ59zJcCAwEAAQ==\n-----END PUBLIC KEY-----\n"
+	// block, _ := pem.Decode([]byte(msg))
+	// color.HiMagenta(string(block.Type))
+	// // color.HiMagenta(string(block.Bytes))
+	// publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	// if err != nil {
+	// 	color.Red(err.Error())
+	// }
+	// color.HiMagenta(fmt.Sprintf("%s", publicKey))
+	// // color.HiMagenta(fmt.Sprintf("%T", publicKey)) // *rsa.PublicKey
 }
--- backend/utils/x509.go
+++ backend/utils/x509.go
@@ -1,21 +1,28 @@
 package utils
 
 import (
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/fatih/color"
 )
 
-var fakeSalt = "meiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiro"
+// not used
+// var fakeSalt = "meiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiromeiro"
 
+// not used
 type loopReader struct {
 	s []byte
 	p int
 }
 
+// not used
 func (r *loopReader) Read(b []byte) (n int, err error) {
 	l := len(r.s)
 	p := 0
@@ -29,6 +36,7 @@ func (r *loopReader) Read(b []byte) (n int, err error) {
 	return p, nil
 }
 
+// parse pem string to public key
 func ParsePublicKey(publicKey string) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode([]byte(publicKey))
 	pk, err := x509.ParsePKIXPublicKey(block.Bytes)
@@ -38,20 +46,81 @@ func ParsePublicKey(publicKey string) (*rsa.PublicKey, error) {
 	return pk.(*rsa.PublicKey), nil
 }
 
-func GenerateKey(salt string) *rsa.PrivateKey {
-	privateKey, err := rsa.GenerateKey(&loopReader{[]byte(fakeSalt), 0}, 512)
+// parse pem string to private key
+func ParsePrivateKey(privateKey string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	pk, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return pk.(*rsa.PrivateKey), nil
+}
+
+// create a new private key
+func GenerateKey() *rsa.PrivateKey {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
 	if err != nil {
 		color.Red(err.Error())
 	}
 	return privateKey
 }
 
+// marshal public key to pem format
 func MarshalPublicKey(privateKey *rsa.PrivateKey) []byte {
-	key, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
 	if err != nil {
 		color.Red(err.Error())
 		return nil
 	}
-	return key
+	publicPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: bytes,
+		},
+	)
+	return publicPem
+}
 
+// marshal private key to pem format
+func MarshalPrivateKey(privateKey *rsa.PrivateKey) []byte {
+	bytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	if err != nil {
+		color.Red(err.Error())
+		return nil
+	}
+	privatePem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PRIVATE KEY",
+			Bytes: bytes,
+		},
+	)
+	return privatePem
+}
+
+func ReadKeyFromFile(path string) (*rsa.PrivateKey, error) {
+	return readKeyFromFile(path)
+}
+
+// read pem file from path, create file when it not exists
+func readKeyFromFile(path string) (*rsa.PrivateKey, error) {
+	if privateKeyPem, err := ioutil.ReadFile(path); err != nil {
+		privateKey := GenerateKey()
+		data := MarshalPrivateKey(privateKey)
+		if data == nil {
+			return nil, errors.New("data is nil")
+		}
+		err = os.WriteFile(path, data, 0644)
+		// fmt.Println(err.Error()) // The system cannot find the file specified.
+		if err != nil {
+			return nil, err
+		}
+		return privateKey, nil
+	} else {
+		privateKey, err := ParsePrivateKey(string(privateKeyPem))
+		if err != nil {
+			color.Red(err.Error())
+			return nil, err
+		}
+		return privateKey, nil
+	}
 }
