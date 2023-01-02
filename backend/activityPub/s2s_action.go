f534b6075ab9e666d5991bc81eb88907729b1734
hana-ame <luminovoez@outlook.com>
2023-01-02 14:23:17 +0800

    as, should be test

---
+++ backend/activityPub/s2s_action.go
@@ -0,0 +1,3 @@
+package activityPub
+
+func Follow()
--- backend/activityPub/s2s_interface.go
+++ backend/activityPub/s2s_interface.go
@@ -14,19 +14,14 @@ import (
 // /users/{username}/
 // /users/{username}
 func Users(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("???")
 	vars := mux.Vars(r)
 	username, ok := vars["username"]
 	if !ok {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
-	// fmt.Println("path", username, ok)
 
-	// fmt.Println(r.Header["Accept"])
-
-	// if accept not match return 302
-	// if accept := r.Header.Get("Accept"); accept != "application/activity+json" && accept != "application/ld+json" {
+	// check the header, if not s2s request, redirect to normal profile
 	if accept := r.Header.Get("Accept"); strings.HasPrefix(accept, "application/activity+json") && strings.HasPrefix(accept, "application/ld+json") {
 		http.Redirect(w, r, fmt.Sprintf("https://%s/@%s", Domain, username), http.StatusFound)
 	}
@@ -47,16 +42,9 @@ func Users(w http.ResponseWriter, r *http.Request) {
 // /{username}/inbox
 // /{username}/inbox/
 func Inbox(w http.ResponseWriter, r *http.Request) {
+	// only POST
 	var err error
-	color.Green(fmt.Sprint(r))
-	text, err := io.ReadAll(r.Body)
-	if err != nil {
-		color.Red(err.Error())
-		return
-	}
-	color.Yellow(string(text))
-	return
-	// debug
+	color.Green(fmt.Sprint(r)) // debug
 
 	r.Host = Domain
 
@@ -65,35 +53,26 @@ func Inbox(w http.ResponseWriter, r *http.Request) {
 	if !ok {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
-	_ = username
+	// redirect to profile page if not s2s action
+	if r.Method != "POST" {
+		http.Redirect(w, r, fmt.Sprintf("/@%s", username), http.StatusMovedPermanently)
+	}
 
+	// httpsig
 	err = verify(r)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	// pk, err := utils.ReadKeyFromFile("privatekey.pem")
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-	// fmt.Println(pk)
-	// key := utils.GenerateKey("")
-	// color.HiMagenta(fmt.Sprintf("%s", *key))
-	// keyBytes := utils.MarshalPublicKey(key)
-	// color.HiMagenta(string(keyBytes))
-	// return
+	// verify success
 
-	// msg := "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvHKTk7Nm8TFF2RUX+MwQ\n69hPKqcKE4JcRVx8eJW5ApSlQs4pu58wQO7yD+QvYIb4IKKOajQhKaaf3vCR0SWm\nwHLVtFn2Iwzm22VFR897eIKCN8DFGT4Nkq3/BiX8ZXb4NbtX5hry/Bj7b2MbDgfX\nptcoafxjofHUkqyZ5VcwgT+ctPjqAaiAHYmnxq4HPoxX5quuvNAe8Gl2Ij+ErE5v\n8Z/jXTMkOQJ0TBPCCSB/PTF0SSPRlwbyZVxXx/dTS/P5ewBO5KPJk9Ii/TDXSDFk\nJKmXxVd7kUIszAQ4qvg97mIi/L8TQf4g6D4Cj0EpbtRZnpv59vvoN7AcrgmKo0CI\n6J08wen9pGpiBp/gXs83mx6YY63SIqLlWEGpEgRZnrpe4nW24N0G1puiTSAiG3o2\n+loXh3J7b4fbcC/UR0pxLLtnhcYXhxUEyfb0d8DV8KjC4GlX9eiDmx0P5fSsUPdc\nOfHeKCKMLnI1FYbTEn+Ed7dYG4RwkXgzJPpigwHIWO2oqaGqWGt2UeOTr+HzbTqW\nOKw+tk1M/3X451DjWTmoGzLPDdoakB8pFyQ8YrrUcpUpLn9n4Susr2deek3UwfgK\nl23OwSttCkw4vs6+p063X3g0NwhWvZYDS/hJDJNbs33LHErfsiUidAmz8yEMsY0X\nKtdeL3dq3dvXYKwyHQ59zJcCAwEAAQ==\n-----END PUBLIC KEY-----\n"
-	// block, _ := pem.Decode([]byte(msg))
-	// color.HiMagenta(string(block.Type))
-	// // color.HiMagenta(string(block.Bytes))
-	// publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	// if err != nil {
-	// 	color.Red(err.Error())
-	// }
-	// color.HiMagenta(fmt.Sprintf("%s", publicKey))
-	// // color.HiMagenta(fmt.Sprintf("%T", publicKey)) // *rsa.PublicKey
+	text, err := io.ReadAll(r.Body)
+	if err != nil {
+		color.Red(err.Error())
+		return
+	}
+	color.Yellow(string(text))
+
 }
 
 // /inbox
